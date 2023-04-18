8e81007b85d6faf810a154e46d11f04471557740
Dapier <josuea501@gmail.com>
2023-04-17 22:02:49 -0600

    adding env

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,7 +1,7 @@
 import nodemailer from 'nodemailer'
 
 const email = process.env.EMAIL
-const pass = process.env.PASS
+const pass = process.env.EMAIL_PASS
 
 export const transporter = nodemailer.createTransport({
   service: 'gmail',
